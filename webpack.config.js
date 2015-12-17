3bd710b0b6b4c62f2f9fdfd73419bcbb74f02ac4
vinogradov <vadim.vinogradov@gmail.com>
2015-12-18 00:35:37 +0400

    eslint: indent warning, npm: latest versions, webpack: simplyfy configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,46 @@
 var path = require('path');
-var webpack = require("webpack");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 var SRC_PATH = path.join(__dirname, 'src');
+var APPLICATION_BUNDLE_FILENAME = 'app.js';
+var CSS_BUNDLE_FILENAME = 'app.css';
+
+var plugins = [
+  new ExtractTextPlugin(CSS_BUNDLE_FILENAME)
+];
+
+var isDist = process.argv[2] === '--dist';
+
+if (isDist) {
+  var webpack = require("webpack");
+  
+  var uglifyJsPlugin = new webpack.optimize.UglifyJsPlugin({
+      mangle: true,
+      comments: false
+  });
+
+  plugins.push(uglifyJsPlugin);
+}
 
 module.exports = {
     context: SRC_PATH,
     entry: "./entry",
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: "app.js"
+        filename: APPLICATION_BUNDLE_FILENAME
     },
     module: {
+      // loaders are loaded from bottom to top
       loaders: [{
           loader: 'babel',
           test: /\.js|jsx$/,
-          include: SRC_PATH,
+          include: SRC_PATH, // other paths are ignored
           query: {
               presets: ['es2015', 'react']
           }
          },{
+          // ESLint should be before any transpiling tools.
+          // Or use preLoaders section to check source files, not modified by other loaders (like babel-loader)
           loader: 'eslint',
           test: /\.js|jsx$/,
           include: SRC_PATH
@@ -28,7 +50,20 @@ module.exports = {
           include: SRC_PATH
         }],
     },
-    plugins: [
-        new ExtractTextPlugin("app.css")
-    ]
+    plugins: plugins
 }
+
+// in case of NODE API (http://webpack.github.io/docs/node.js-api.html):
+
+// webpack({
+  
+// }, function(err, stats) {
+// }).watch({ // watch options:
+//   debug: true,
+//     aggregateTimeout: 300, // wait so long for more changes
+//     poll: true // use polling instead of native watchers
+//     // pass a number to set the polling interval
+
+// }, function(err, stats) {
+//     console.log(stats.toString({colors: true}));
+// });
