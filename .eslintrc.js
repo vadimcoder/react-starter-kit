a3d4a6a414af99a646910a678f8a78c08afa83a8
vinogradov <vadim.vinogradov@gmail.com>
2017-05-14 21:20:44 +0400

    Polish eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
+  extends: 'airbnb',
+  env: {
+    browser: true
+  },
   rules: {
     'object-curly-spacing': ['warn', 'never'],
     'comma-dangle': ['warn', 'never'],
     'max-len': ['warn', 120],
     'react/jsx-filename-extension': ['off']
-  },
-  env: {
-    browser: true
-  },
-  extends: 'airbnb'
+  }
 };
